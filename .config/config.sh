bfe97a4796693245db277bff031cbabf5c95f587
Tenzin Gangshar <thetenzin21@gmail.com>
2024-04-26 15:07:53 -0400

    new folder for misc

--- .config/config.sh
+++ .config/config.sh
@@ -3,3 +3,10 @@
 # GoLang
 go install github.com/cosmtrek/air@latest
 go install goland.org/x/tools/gopls@latest
+
+# install gcc from tmd
+
+# install templ, mv to repo if trouble with alias
+go install github.com/a-h/templ/cmd/templ@latest
+
+# install tailwindcss w/out npm
